8a2b5b0560e1ec6cb5b09f59a95ac82735fe6f67
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-04-26 09:45:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-/*
-UUID only works if the user is on a specific browser, 
-say for example, the user switches web browsers, the uuid will be different
-*/
-
 const h1 = document.querySelector('h1')
 
-function getUUID(){
-    try{
-        return new DeviceUUID().get();
-    } catch(err){}
-}
+const fpPromise = import('https://openfpcdn.io/fingerprintjs/v4')
+  .then(FingerprintJS => FingerprintJS.load())
 
-const uuid = getUUID()
-h1.innerText = uuid.toString()
+// Get the visitor identifier when you need it.
+fpPromise
+  .then(fp => fp.get())
+  .then(result => {
+    // This is the visitor identifier:
+    const uuid = result.visitorId
+    h1.innerText = uuid.toString()
 
-fetch('./valid-uuid.json')
-.then((res) => res.json())
-.then((data = {}) => {
-    console.log(data)
-    if (data.includes(uuid)){
-        window.location.href = "/main.html"
-    } else{
-        alert('not valid')
-    }
-})
+    fetch('./valid-uuid.json')
+    .then((res) => res.json())
+    .then((data = {}) => {
+        console.log(data)
+        if (data.includes(uuid)){
+            alert('valid')
+            window.location.href = "/main.html"
+        } else{
+            alert('not valid')
+        }
+    })
+
+  })
+  .catch(error => console.error(error))
