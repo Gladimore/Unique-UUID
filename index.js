65d8aca7260187fb4d37470e6e7aa9fcda87c92f
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-04-25 22:51:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+UUID only works if the user is on a specific browser, 
+say for example, the user switches web browsers, the uuid will be different
+*/
+
+function getUUID(){
+    let uuid
+
+    try{
+        uuid = new DeviceUUID().get();
+    } catch(err){}
+
+    return uuid
+}
+
+const uuid = getUUID()
+console.log(uuid)
+
+fetch('./valid-uuid.json')
+.then((res) => res.json())
+.then((data = {}) => {
+    if (data.includes(uuid)){
+        alert('valid')
+    }
+})
